b55388300b6ffb9b160c875c4432819b68afef18
0xz00n <0xz00n@gmail.com>
2019-02-15 00:48:47 -0600

    Beginning changes to overall structure

---
+++ Dev/modules/help.py
@@ -0,0 +1,69 @@
+import discord
+
+class HelpMenus:
+#    def __init__(self):
+    def genHelp():
+        em = discord.Embed(
+            title='Great Owl Help',
+            description="""Help Commands:
+                        !help: Displays this help message
+                        !rolehelp: Role command help
+                        !shiphelp: Ship information help\n
+                        Website Link Commands:
+                        !coriolis: Coriolis website
+                        !eddb: Elite Dangerous Database website
+                        !edrefcard: EDRefCard website
+                        !edsm: Elite Dangerous Star Map website
+                        !inara: INARA website\n
+                        Other Commands:
+                        !vopals: Our stance on core mining
+                        !materials: Spreadsheet for engineering materials and their locations""",
+            color=0xff7700
+        )
+        em.set_image(
+            url='https://i.imgur.com/7yG4taS.jpg'
+        )
+        await client.send_message(message.channel, embed=em)
+
+    def roleHelp():
+        em = discord.Embed(
+            title='Great Owl Role Help',
+            description="""!pc: Add yourself to the PC role
+                        !xb1: Add yourself to the Xbox 1 role
+                        !ps4: Add yourself to the PS4 role\n
+                        If you run a command while already assigned to its role, you will be asked if you wish to be removed from the role.""",
+            color=0xff7700
+        )
+        em.set_image(
+            url='https://i.imgur.com/7yG4taS.jpg'
+        )
+        await client.send_message(message.channel, embed=em)
+
+    def shipHelp():
+        em = discord.Embed(
+            title='Great Owl Ship Help',
+            description="""Information pertaining to each ship listed:
+                        !sidewinder: Sidewinder
+                        !eagle: Eagle
+                        !hauler: Hauler
+                        !adder: Adder
+                        !ieagle: Imperial Eagle
+                        !viper3: Viper Mk.III
+                        !cobra3: Cobra Mk.III
+                        !viper4: Viper Mk.IV
+                        !dbscout: Diamondback Scout
+                        !cobra4: Cobra Mk.IV
+                        !type6: Type-6 Transporter
+                        !dolphin: Dolphin
+                        !dbexplorer: Diamondback Explorer
+                        !icourier: Imprerial Courier
+                        !keelback: Keelback
+                        !aspscout: Asp Scout
+                        !vulture: Vulture
+                        !aspx: Asp Explorer""",
+            color=0xff7700
+        )
+        em.set_image(
+            url='https://i.imgur.com/7yG4taS.jpg'
+        )
+        await client.send_message(message.channel, embed=em)
---
+++ Dev/modules/roles.py
@@ -0,0 +1,62 @@
+import discord
+
+class roles:
+    def pcRole(discord, client):
+        role = discord.utils.get(message.server.roles, name='PC')
+        if discord.utils.get(message.author.roles, name='PC'):
+            msg = 'You already have the PC role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content.lower() == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the PC role.')
+            elif reply.content.lower() == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content != None:
+                await client.send_message(message.channel, 'Unrecognized response. Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the PC role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
+
+    def xb1Role(discord, client):
+        role = discord.utils.get(message.server.roles, name='XBOX 1')
+        if discord.utils.get(message.author.roles, name='XBOX 1'):
+            msg = 'You already have the Xbox 1 role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content.lower() == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the Xbox 1 role.')
+            elif reply.content.lower() == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content != None:
+                await client.send_message(message.channel, 'Unrecognized response. Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the Xbox 1 role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
+
+    def ps4Role(discord, client):
+        role = discord.utils.get(message.server.roles, name='PS4')
+        if discord.utils.get(message.author.roles, name='PS4'):
+            msg = 'You already have the PS4 role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content.lower() == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the PS4 role.')
+            elif reply.content.lower() == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content != None:
+                await client.send_message(message.channel, 'Unrecognized response. Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the PS4 role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
